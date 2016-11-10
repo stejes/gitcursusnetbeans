950a36e7e072e864dba15bf631d9110a71c57697
StevenJ <s.jespers@telenet.be>
2016-11-10 09:24:55 +0100

    bonjour tout le monde

--- src/gitcursus/GitCursus.java
+++ src/gitcursus/GitCursus.java
@@ -16,7 +16,7 @@ public class GitCursus {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Bonjour");
+        System.out.println("Bonjour tout le monde");
     }
     
 }
